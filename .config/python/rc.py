07497bc1ce9b64b660181fe74a271ec0611f0200
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-02-09 20:48:58 +0200

    Remove '*-imports' in python'c rc.

--- .config/python/rc.py
+++ .config/python/rc.py
@@ -1,14 +1,13 @@
-from math import *
-from pylab import *
-
 import sys
 import matplotlib
 import readline
+import math
+import pylab
 
 readline.parse_and_bind('"\C-l": "    "')
 
 matplotlib.rcParams['backend'] = 'TkAgg'
-ion()
+pylab.ion()
 
 sys.ps1 = '\001\033[01;33m\002|>\001\033[00m\002'
 sys.ps2 = '\001\033[01;33m\002->\001\033[00m\002'
