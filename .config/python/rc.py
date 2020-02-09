bf112ec6127728a08eb620608b00ad9202b9c4d8
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-02-09 17:02:49 +0200

    Split the readline macros between apps.

--- .config/python/rc.py
+++ .config/python/rc.py
@@ -1,13 +1,14 @@
 from math import *
 from pylab import *
 
+import sys
 import matplotlib
+import readline
+
+readline.parse_and_bind('"\C-l": "    "')
 
 matplotlib.rcParams['backend'] = 'TkAgg'
-
 ion()
-
-import sys
 
 sys.ps1 = '\001\033[01;33m\002|>\001\033[00m\002'
 sys.ps2 = '\001\033[01;33m\002->\001\033[00m\002'
