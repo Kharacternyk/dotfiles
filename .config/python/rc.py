51cf6c2accc2f5842b31d8c83acb624a43eae310
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-03-20 20:41:40 +0200

    Separate different Python REPL workflows.

--- .config/python/rc.py
+++ .config/python/rc.py
@@ -1,13 +1,25 @@
-import sys
-import matplotlib
-import readline
-import math
-import pylab
+import sys, os, readline
 
 readline.parse_and_bind('"\C-l": "    "')
-readline.parse_and_bind('"\C-g": "from gi.repository import "')
-
-pylab.ion()
 
 sys.ps1 = '\001\033[01;33m\002|>\001\033[00m\002'
 sys.ps2 = '\001\033[01;33m\002->\001\033[00m\002'
+
+class Nltk:
+    def __init__(self):
+        exec('from nltk import *', globals())
+
+class Calc:
+    def __init__(self):
+        exec('from math import *', globals())
+
+class Math:
+    def __init__(self):
+        exec('import math', globals())
+        exec('from matplotlib.pyplot import *', globals())
+        exec('from numpy import *', globals())
+        exec('ion()', globals())
+
+class Gtk:
+    def __init__(self):
+        exec('from gi.repository import Gtk, Gdk, Vte', globals())
